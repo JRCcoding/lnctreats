0825a304f89665b9198c9997c04fec3a4a517190
Josh Claxton <jrccode@outlook.com>
2022-12-11 16:00:30 -0600

    serverjs fix for dev

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ const __dirname = dirname(__filename)
 // }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('../client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
