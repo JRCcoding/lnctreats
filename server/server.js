0657c26134c66903840cb2d77d2d1ea259ef128a
Josh Claxton <jrccode@outlook.com>
2022-12-11 13:58:54 -0600

    replaced another 2 frontend with client naming

--- server/server.js
+++ server/server.js
@@ -44,9 +44,9 @@ const __dirname = dirname(__filename)
 // }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 const PORT = process.env.PORT || 10000
