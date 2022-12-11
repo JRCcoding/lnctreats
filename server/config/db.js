43bed7898bd893de2b08413002dd8e2509113e21
Josh Claxton <jrccode@outlook.com>
2022-12-11 17:26:41 -0600

    colors for console

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 
 const connectDB = async () => {
   const URI = process.env.MONGO_URI
   try {
     const conn = await mongoose.connect(URI)
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.brightGreen)
   } catch (error) {
     console.error(`Error: ${error.message}`)
     process.exit(1)
--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ const __dirname = dirname(__filename)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   })
 }
 const PORT = process.env.PORT || 10000
