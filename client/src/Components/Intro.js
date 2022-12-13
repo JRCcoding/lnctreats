392e0f83e65fefad00108a2da1b69322ab900d25
Josh Claxton <jrccode@outlook.com>
2022-12-12 22:26:53 -0600

    clean shadows import images for mongodb

--- client/src/Components/Intro.js
+++ client/src/Components/Intro.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import asyncHandler from 'async-handler'
 import { Container } from 'react-bootstrap'
 import About from './About'
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit'
@@ -6,12 +8,22 @@ import 'animate.css'
 import Prod1 from '../Images/product1.jpg'
 import Prod2 from '../Images/product2.jpg'
 import Prod3 from '../Images/product3.jpg'
+// import ProductCarousel from './ProductCarousel'
 
 const Intro = () => {
+  // const [products, setProducts] = useState([])
+
+  // useEffect(() => {
+  //   const fetchProducts = asyncHandler(async () => {
+  //     const { data } = await axios.get('/api/products')
+  //     setProducts(data)
+  //   })
+  //   fetchProducts()
+  // }, [])
   return (
     <div className='intro mb-5 pt-5' id='intro'>
       <Container>
-        <MDBCarousel className='carousel mt-5 shadow-lg'>
+        <MDBCarousel className='carousel mt-5'>
           <MDBCarouselItem
             className='w-100 d-block'
             itemId={1}
@@ -46,6 +58,20 @@ const Intro = () => {
           </MDBCarouselItem>
         </MDBCarousel>
 
+        {/*<MDBCarousel className='bg-dark'>
+          {products &&
+            products.map((product) => (
+              <MDBCarousel.Item>
+                {/* <Link to={`/product/${product._id}`}> 
+                <MDBCarouselItem src={product.img} alt={product.title} fluid />
+                <MDBCarouselItem className='carousel-caption'>
+                  <h3>{product.name}</h3>
+                </MDBCarouselItem>
+                {/* </Link> 
+              </MDBCarousel.Item>
+            ))}
+        </MDBCarousel>
+            */}
         <About />
       </Container>
     </div>
