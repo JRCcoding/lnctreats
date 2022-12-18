1e91f4484b73ba257f993ed8a3e60e3668ec68dd
Josh Claxton <jrccode@outlook.com>
2022-12-18 00:20:57 -0600

    cleanup and prep from contact form

--- client/src/Screens/homeScreen.js
+++ client/src/Screens/homeScreen.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Intro from '../Components/Intro'
 import SellingPoints from '../Components/SellingPoints'
-// import { Reveal } from 'react-reveal'
+// import Contact from '../Components/Contact'
 
 const HomeScreen = () => {
   return (
     <>
-      {/* <Reveal effect='animate__animated animate__fadeInUp' cascade> */}
       <Intro />
       <SellingPoints />
-      {/* </Reveal> */}
+      {/* <Contact /> */}
     </>
   )
 }
--- client/src/Screens/productsScreen.js
+++ client/src/Screens/productsScreen.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Products from '../Components/Products'
-// import Reveal from 'react-reveal'
 
 const ProductsScreen = () => {
   return (
     <>
-      {/* <Reveal effect='animate__animated animate__fadeInUp' cascade> */}
       <Products />
-      {/* </Reveal> */}
     </>
   )
 }
