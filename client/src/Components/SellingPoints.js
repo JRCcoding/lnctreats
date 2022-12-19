0e1345f859fe1fb5b39f25f1d73e5235346f43f5
Josh Claxton <jrccode@outlook.com>
2022-12-19 14:20:35 -0600

    a11y cleanups and mobile views

--- client/src/Components/SellingPoints.js
+++ client/src/Components/SellingPoints.js
@@ -10,10 +10,10 @@ const SellingPoints = () => {
   return (
     <Container className='card_container'>
       <AnimationOnScroll animateIn='animate__fadeInUp' animateOnce='true'>
-        <Card className='about_card rounded-top' id='top_card'>
+        <Card className='about_card' id='top_card'>
           <Card.Body className='bg-Background about_card_body'>
             <img src={cakeicon} className='card_img' alt='cake icon'></img>
-            <h1 className='card_title bg-Background  font-Pedestria'>Cakes</h1>
+            <h1 className='card_title'>Cakes</h1>
             <p className='card_text font-Multi'>
               Simple cakes to fully customized, contact me below to talk about
               pricing and work with your budget!
@@ -21,31 +21,34 @@ const SellingPoints = () => {
           </Card.Body>
         </Card>
       </AnimationOnScroll>
+      <AnimationOnScroll animateIn='animate__fadeInUp' animateOnce='true'>
+        <Card className='about_card_reverse'>
+          <Card.Body className='bg-Background about_card_body_reverse'>
+            <img
+              src={snackicon}
+              className='card_img_reverse'
+              alt='snack icon'
+            ></img>
+            <h1 className='card_title_reverse'>Snacks</h1>
+            <p className='card_text_reverse font-Multi'>
+              Huge variety of snacks from gummies to Kickin' Munchy Mix
+            </p>
+          </Card.Body>
+        </Card>
+      </AnimationOnScroll>
 
-      <Card className='about_card_reverse'>
-        <Card.Body className='bg-Background about_card_body_reverse'>
-          <img
-            src={snackicon}
-            className='card_img_reverse'
-            alt='snack icon'
-          ></img>
-          <h1 className='card_title_reverse font-Pedestria'>Snacks</h1>
-          <p className='card_text_reverse font-Multi'>
-            Huge variety of snacks from gummies to Kickin' Munchy Mix
-          </p>
-        </Card.Body>
-      </Card>
-
-      <Card className='about_card'>
-        <Card.Body className='bg-Background about_card_body'>
-          <img src={catericon} className='card_img' alt='cake icon'></img>
-          <h1 className='card_title  font-Pedestria'>Catering</h1>
-          <p className='card_text  font-Multi'>
-            From small meetings to all out BASHes, I can help you make your
-            occassion that much more special
-          </p>
-        </Card.Body>
-      </Card>
+      <AnimationOnScroll animateIn='animate__fadeInUp' animateOnce='true'>
+        <Card className='about_card'>
+          <Card.Body className='bg-Background about_card_body'>
+            <img src={catericon} className='card_img' alt='cake icon'></img>
+            <h1 className='card_title'>Catering</h1>
+            <p className='card_text  font-Multi'>
+              From small meetings to all out BASHes, I can help you make your
+              occassion that much more special
+            </p>
+          </Card.Body>
+        </Card>
+      </AnimationOnScroll>
     </Container>
   )
 }
