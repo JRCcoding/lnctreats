f7032c401cee67c383b07dc91857049421f68b24
Josh Claxton <jrccode@outlook.com>
2022-12-23 15:49:47 -0600

    clean product page and ready for bgptrn

--- client/src/Components/Product.js
+++ client/src/Components/Product.js
@@ -1,50 +1,61 @@
 import React, { useEffect, useState } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Loader from '../Components/Loader'
-// import { listProductDetails } from '../Actions/productActions'
+import { listProducts } from '../Actions/productActions'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import '../Styles/Product.css'
 const Product = () => {
   const [product, setProduct] = useState({})
   const { id } = useParams()
+  const dispatch = useDispatch()
 
+  const productList = useSelector((state) => state.productList)
+  const { loading, error } = productList
   useEffect(() => {
-    const fetchBlogpost = async () => {
+    const fetchProduct = async () => {
       const { data } = await axios.get(`/api/products/${id}`)
 
       setProduct(data)
     }
-    fetchBlogpost()
-  })
+    fetchProduct()
 
+    dispatch(listProducts())
+  }, [dispatch, id])
+  // })
   return (
     <div>
       <>
-        <Row>
-          <Col md={6}>
-            <Image
-              src={product.img}
-              alt={product.title}
-              fluid
-              className='prod_img'
-            />
-          </Col>
-          <Col md={3}>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <h3>{product.title}</h3>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Description: </strong> {product.description}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>More Info: </strong> {product.additional}
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-        </Row>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <h3>{error}</h3>
+        ) : (
+          <Row>
+            <Col md={6}>
+              <Image
+                src={product.img}
+                alt={product.title}
+                fluid
+                className='prod_img'
+              />
+            </Col>
+            <Col md={3}>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <h3>{product.title}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Description: </strong> {product.description}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>More Info: </strong> {product.additional}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        )}
       </>
     </div>
   )
--- client/src/Screens/productScreen.js
+++ client/src/Screens/productScreen.js
@@ -2,7 +2,12 @@ import React from 'react'
 import Product from '../Components/Product'
 
 const ProductScreen = () => {
-  return <Product />
+  return (
+    // <div className='background_pattern'>
+    <div>
+      <Product />
+    </div>
+  )
 }
 
 export default ProductScreen
