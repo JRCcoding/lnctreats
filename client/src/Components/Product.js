09ad04a741f577f5352f4fd222ec95da9b182a85
Josh Claxton <jrccode@outlook.com>
2022-12-23 15:40:51 -0600

    more info on prod indiv pages

--- client/src/Components/Product.js
+++ client/src/Components/Product.js
@@ -5,22 +5,8 @@ import Loader from '../Components/Loader'
 // import { listProductDetails } from '../Actions/productActions'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
-const Product = ({ match }) => {
-  // const dispatch = useDispatch()
-
-  // const productDetails = useSelector((state) => state.productDetails)
-  // const { loading, error, product } = productDetails
-
-  // useEffect(() => {
-  //   // if (successProductReview) {
-  //   //   setRating(0)
-  //   //   setComment('')
-  //   // }
-  //   if (!product.id || product._id !== match.params.id) {
-  //     dispatch(listProductDetails(match.params.id))
-  //   }
-  // }, [dispatch, match])
-
+import '../Styles/Product.css'
+const Product = () => {
   const [product, setProduct] = useState({})
   const { id } = useParams()
 
@@ -38,81 +24,25 @@ const Product = ({ match }) => {
       <>
         <Row>
           <Col md={6}>
-            <Image src={product.img} alt={product.title} fluid />
+            <Image
+              src={product.img}
+              alt={product.title}
+              fluid
+              className='prod_img'
+            />
           </Col>
           <Col md={3}>
             <ListGroup variant='flush'>
               <ListGroup.Item>
                 <h3>{product.title}</h3>
               </ListGroup.Item>
-              {/* <ListGroup.Item>
-                  <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} reviews`}
-                  />
-                </ListGroup.Item> */}
-              {/* <ListGroup.Item>Price: ${product.price}</ListGroup.Item> */}
               <ListGroup.Item>
-                Description: {product.description}
+                <strong>Description: </strong> {product.description}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>More Info: </strong> {product.additional}
               </ListGroup.Item>
             </ListGroup>
-          </Col>
-          <Col md={3}>
-            {/* <Card> */}
-            {/* <ListGroup variant='flush'>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${product.price}</strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item> */}
-
-            {/* <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                      </Col>
-                    </Row>
-                  </ListGroup.Item> */}
-
-            {/* {product.countInStock > 0 && (
-                    <ListGroup.Item>
-                      <Row>
-                        <Col>Qty</Col>
-                        <Col>
-                          <Form.Control
-                            as='select'
-                            value={qty}
-                            onChange={(e) => setQty(e.target.value)}
-                          >
-                            {[...Array(product.countInStock).keys()].map(
-                              (x) => (
-                                <option key={x + 1} value={x + 1}>
-                                  {x + 1}
-                                </option>
-                              )
-                            )}
-                          </Form.Control>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  )} */}
-
-            {/* <ListGroup.Item>
-                    <Button
-                      onClick={addToCartHandler}
-                      className='btn-block'
-                      type='button'
-                      disabled={product.countInStock === 0}
-                    >
-                      Add To Cart
-                    </Button>
-                  </ListGroup.Item> */}
-            {/* </ListGroup> */}
-            {/* </Card> */}
           </Col>
         </Row>
       </>
