04d5d0f541bbaef27fe1d3d623cd6bbb1a42db54
JRCcoding <jrccode@outlook.com>
2023-01-09 09:30:44 -0700

    valentines deployment

--- client/src/Components/Cart.js
+++ client/src/Components/Cart.js
@@ -107,29 +107,34 @@ const Cart = ({ location, history }) => {
                 ))}
               </ListGroup>
             )}
+            <hr></hr>
+            {cartItems.product !== '63bc3572245fdfc40c478e9d' && (
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col md={2}>
+                      <LinkContainer to={'/product/63bc3572245fdfc40c478e9d'}>
+                        <Image
+                          src='https://raw.githubusercontent.com/JRCcoding/lnctreats/development/client/src/Images/webp/xsmall-bouquet.webp'
+                          alt='Small Bouquet'
+                          fluid
+                          rounded
+                        />
+                      </LinkContainer>
+                    </Col>
 
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <Row>
-                  <Col md={2}>
-                    <Image
-                      src='https://raw.githubusercontent.com/JRCcoding/lnctreats/development/client/src/Images/webp/xsmall-bouquet.webp'
-                      alt='Small Bouquet'
-                      fluid
-                      rounded
-                    />
-                  </Col>
-                  <Col md={4}>
-                    <LinkContainer to={'/product/63bc3572245fdfc40c478e9d'}>
-                      <strong className='clickable'>
-                        Add a Small Candy Bouquet!
-                      </strong>
-                    </LinkContainer>
-                  </Col>
-                  <Col md={2}>$10.00</Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
+                    <Col md={4}>
+                      <LinkContainer to={'/product/63bc3572245fdfc40c478e9d'}>
+                        <strong className='clickable'>
+                          Add a Small Candy Bouquet! ❤
+                        </strong>
+                      </LinkContainer>
+                    </Col>
+                    <Col md={2}>$10.00</Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            )}
           </Col>
           <Col md={4}>
             <Card>
