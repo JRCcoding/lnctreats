aa1891024820a3eaa41d4ca47ccd25df36b2df13
Josh Claxton <jrccode@outlook.com>
2023-03-18 00:18:26 -0500

    cakes form only for cakes

--- client/src/Components/Product.js
+++ client/src/Components/Product.js
@@ -72,105 +72,8 @@ const Product = ({ history }) => {
           <Message variant='danger'>{error}</Message>
         ) : (
           <MDBCard className='prod_info_box pr-2'>
-            {product && !product.category2 && (
-              <Row>
-                <Col md={6}>
-                  <LinkContainer to='/products'>
-                    <button className='backprod_button absolute top-0'>
-                      GO BACK
-                    </button>
-                  </LinkContainer>
-
-                  <Image
-                    src={product.img}
-                    alt={product.title}
-                    fluid
-                    className='prod_img'
-                  />
-                </Col>
-                <Col md={6}>
-                  <ListGroup variant='flush' className='mr-2'>
-                    <ListGroup.Item>
-                      <h3>{product.title}</h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <strong>Description: </strong> {product.description}
-                    </ListGroup.Item>
-                    {/* <ListGroup.Item>
-                    <strong>More Info: </strong> {product.additional}
-                  </ListGroup.Item> */}
-                    <ListGroup.Item>
-                      <strong>Price: $</strong>
-                      {product.price}
-                    </ListGroup.Item>
-
-                    {product.countInStock < 1 && (
-                      <ListGroup.Item>
-                        <LinkContainer to='/contact'>
-                          <button className='contact_button'>
-                            CONTACT TO ORDER
-                          </button>
-                        </LinkContainer>
-                      </ListGroup.Item>
-                    )}
-                    {product.countInStock > 0 && (
-                      <ListGroup>
-                        <ListGroup.Item>
-                          Any custom orders or over limit orders on website
-                          please
-                          <LinkContainer to='/contact'>
-                            <h6 className='inline clickable'> CONTACT ME</h6>
-                          </LinkContainer>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                          <Row>
-                            <Col>
-                              <h5>Quantity</h5>
-                            </Col>
-                            <Col>
-                              <Form.Control
-                                as='select'
-                                value={qty}
-                                onChange={(e) => setQty(e.target.value)}
-                              >
-                                {[
-                                  ...Array(Number(product.countInStock)).keys(),
-                                ].map((x) => (
-                                  <option key={x + 1} value={x + 1}>
-                                    {x + 1}
-                                  </option>
-                                ))}
-                              </Form.Control>
-                            </Col>
-                          </Row>
-                        </ListGroup.Item>
-                        <Form.Group
-                          className='mb-3'
-                          controlId='additional'
-                          value={additional}
-                          onChange={(e) => setAdditional(e.target.value)}
-                        >
-                          <Form.Label>Additions and Customizations:</Form.Label>
-                          <Form.Control type='textbox' placeholder='' />
-                        </Form.Group>
-                        <ListGroup.Item>
-                          <button
-                            onClick={addToCartHandler}
-                            className='addcart_button'
-                            type='button'
-                          >
-                            ADD TO CART
-                          </button>
-                        </ListGroup.Item>
-                      </ListGroup>
-                    )}
-                  </ListGroup>
-                </Col>
-              </Row>
-            )}
-
             {/* SPLIT FOR CAT2 */}
-            {product && product.category2 && (
+            {product.category2 === 'cakes' ? (
               <Row>
                 <Col md={6}>
                   <LinkContainer to='/products'>
@@ -375,6 +278,101 @@ const Product = ({ history }) => {
                       </ListGroup.Item>
                     </ListGroup.Item>
                   </Form>
+                </Col>
+              </Row>
+            ) : (
+              <Row>
+                <Col md={6}>
+                  <LinkContainer to='/products'>
+                    <button className='backprod_button absolute top-0'>
+                      GO BACK
+                    </button>
+                  </LinkContainer>
+
+                  <Image
+                    src={product.img}
+                    alt={product.title}
+                    fluid
+                    className='prod_img'
+                  />
+                </Col>
+                <Col md={6}>
+                  <ListGroup variant='flush' className='mr-2'>
+                    <ListGroup.Item>
+                      <h3>{product.title}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <strong>Description: </strong> {product.description}
+                    </ListGroup.Item>
+                    {/* <ListGroup.Item>
+                <strong>More Info: </strong> {product.additional}
+              </ListGroup.Item> */}
+                    <ListGroup.Item>
+                      <strong>Price: $</strong>
+                      {product.price}
+                    </ListGroup.Item>
+
+                    {product.countInStock < 1 && (
+                      <ListGroup.Item>
+                        <LinkContainer to='/contact'>
+                          <button className='contact_button'>
+                            CONTACT TO ORDER
+                          </button>
+                        </LinkContainer>
+                      </ListGroup.Item>
+                    )}
+                    {product.countInStock > 0 && (
+                      <ListGroup>
+                        <ListGroup.Item>
+                          Any custom orders or over limit orders on website
+                          please
+                          <LinkContainer to='/contact'>
+                            <h6 className='inline clickable'> CONTACT ME</h6>
+                          </LinkContainer>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                          <Row>
+                            <Col>
+                              <h5>Quantity</h5>
+                            </Col>
+                            <Col>
+                              <Form.Control
+                                as='select'
+                                value={qty}
+                                onChange={(e) => setQty(e.target.value)}
+                              >
+                                {[
+                                  ...Array(Number(product.countInStock)).keys(),
+                                ].map((x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                ))}
+                              </Form.Control>
+                            </Col>
+                          </Row>
+                        </ListGroup.Item>
+                        <Form.Group
+                          className='mb-3'
+                          controlId='additional'
+                          value={additional}
+                          onChange={(e) => setAdditional(e.target.value)}
+                        >
+                          <Form.Label>Additions and Customizations:</Form.Label>
+                          <Form.Control type='textbox' placeholder='' />
+                        </Form.Group>
+                        <ListGroup.Item>
+                          <button
+                            onClick={addToCartHandler}
+                            className='addcart_button'
+                            type='button'
+                          >
+                            ADD TO CART
+                          </button>
+                        </ListGroup.Item>
+                      </ListGroup>
+                    )}
+                  </ListGroup>
                 </Col>
               </Row>
             )}
