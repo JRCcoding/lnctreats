8ce79420efc18074bbf0b29c2f0e917d6f006375
Josh Claxton <jrccode@outlook.com>
2023-04-08 23:49:10 -0500

    store.js state setup for requests

--- client/src/Screens/cakeSubmitScreen.js
+++ client/src/Screens/cakeSubmitScreen.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
 import { useParams, withRouter } from 'react-router-dom'
+import { createRequest } from '../Actions/requestActions'
 // size,
 //         qty,
 //         date,
@@ -8,21 +10,41 @@ import { useParams, withRouter } from 'react-router-dom'
 //         name,
 //         email,
 const CakeSubmitScreen = ({ location }) => {
-  const { date } = useParams()
-  const orderDate = date
-  const user_name = new URLSearchParams(location.search).get('name')
-  const user_email = new URLSearchParams(location.search).get('email')
+  const { size } = useParams()
+  const itemSize = size
+  const qty = new URLSearchParams(location.search).get('qty')
+  const date = new URLSearchParams(location.search).get('date')
+  const additional = new URLSearchParams(location.search).get('additional')
+  const name = new URLSearchParams(location.search).get('name')
+  const email = new URLSearchParams(location.search).get('email')
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (itemSize) {
+      dispatch(createRequest(itemSize, qty, date, additional, name, email))
+    }
+  }, [dispatch, itemSize, qty, date, additional, name, email])
   return (
     <Container>
       <h5>
-        Thank you for your request, <strong>{user_name}</strong>!{' '}
+        Thank you for your request, <strong>{name}</strong>!{' '}
       </h5>
+      <p>{itemSize}I will get back to you as soon as possible!</p>
       <p>
-        I will get back to you as soon as possible! You placed your order for{' '}
-        <strong>{orderDate}</strong> and you should receive an email at{' '}
-        <strong>{user_email}</strong>. You can reply to email with any further
-        information or questions!
+        {' '}
+        You placed your order for <strong>{date}</strong> and you should receive
+        an email at <strong>{email}</strong>.
       </p>
+      <p>
+        {additional !== 'undefined' ? (
+          <>
+            Here is your provided information: <br />{' '}
+            <i>&nbsp;&nbsp;&nbsp;"{additional}"</i>
+          </>
+        ) : (
+          <></>
+        )}
+      </p>
+      <p> You can reply to email with any further information or questions!</p>
       <p></p>
     </Container>
   )
