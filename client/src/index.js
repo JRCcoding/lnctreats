5f1491a1c02712ef8188f048e82c1da5c818afb2
Josh Claxton <jrccode@outlook.com>
2023-05-07 11:49:50 -0500

    redirect_uri fix

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,7 @@ root.render(
       clientId='EQF56gG62TNgxBy9obK2mKqtKJknBDgd'
       useRefreshTokens={true}
       authorizationParams={{
-        redirect_uri: 'https://www.lnctreats.com',
+        redirect_uri: 'https://lnctreats.com',
         audience: 'https://dev-dstps3q4l34f7d23.us.auth0.com/api/v2/',
         scope:
           'openid profile email read:users read:current_user update:current_user_metadata offline_access',
